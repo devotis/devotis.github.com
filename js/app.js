b33c2fba5241daa6ca2eb60eed61a2ef16d50c25
Christiaan Westerbeek <chris@devotis.nl>
2013-04-04 07:07:08 +0200

    More content copied from devotis.nl

--- js/app.js
+++ js/app.js
@@ -6,14 +6,14 @@ angular.module('site', ['$strap.directives']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/',                            {templateUrl: 'partials/en/home.html',    controller: SiteCtrl}).
-      when('/blog',                        {templateUrl: 'partials/en/blog.html',   controller: SiteCtrl}).
       when('/services/stoic',              {templateUrl: 'partials/en/stoic.html',   controller: SiteCtrl}).
       when('/services/clang',              {templateUrl: 'partials/en/clang.html',   controller: SiteCtrl}).
       when('/services/google-apps',        {templateUrl: 'partials/en/google-apps.html',   controller: SiteCtrl}).
       when('/services/domain-and-hosting', {templateUrl: 'partials/en/domain-and-hosting.html',   controller: SiteCtrl}).
       when('/about/devotis',               {templateUrl: 'partials/en/about-devotis.html',   controller: SiteCtrl}).
       when('/about/christiaan-westerbeek', {templateUrl: 'partials/en/about-chris.html',   controller: SiteCtrl}).
-      when('/about/customers',             {templateUrl: 'partials/en/about-customers.html', controller: SiteCtrl}).
+      when('/blog',                        {templateUrl: 'partials/en/blog.html',   controller: SiteCtrl}).
+      when('/customers',                   {templateUrl: 'partials/en/customers.html', controller: SiteCtrl}).
       when('/contact',                     {templateUrl: 'partials/en/contact.html',   controller: SiteCtrl}).
       otherwise({redirectTo: '/'});
 }]);
