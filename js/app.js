2632cc8a73f7ba732e63ff2c31e4d266d8146989
Christiaan Westerbeek <chris@devotis.nl>
2013-04-12 10:38:07 +0200

    shorter

--- js/app.js
+++ js/app.js
@@ -6,24 +6,21 @@ angular.module('site', ['$strap.directives']).
   config(['$routeProvider', function($routeProvider) {
       
   var p = location.pathname;
-  if (p=='/') {
-      p='/index.html';
-  }
-  
+  p = 'partials'+(p == '/' ? '/index.html' : p);
   
   $routeProvider.
-      when('/',                                {templateUrl: 'partials'+p+'/home.html',    controller: SiteCtrl}).
-      when('/services/stoic',                  {templateUrl: 'partials'+p+'/stoic.html',   controller: SiteCtrl}).
-      when('/services/clang',                  {templateUrl: 'partials'+p+'/clang.html',   controller: SiteCtrl}).
-      when('/services/google-apps',            {templateUrl: 'partials'+p+'/google-apps.html',   controller: SiteCtrl}).
-      when('/services/javascript-development', {templateUrl: 'partials'+p+'/javascript-development.html',   controller: SiteCtrl}).
-      when('/services/sencha',                 {templateUrl: 'partials'+p+'/javascript-development.html',   controller: SiteCtrl}).
-      when('/services/domain-and-hosting',     {templateUrl: 'partials'+p+'/domain-and-hosting.html',   controller: SiteCtrl}).
-      when('/about/devotis',                   {templateUrl: 'partials'+p+'/about-devotis.html',   controller: SiteCtrl}).
-      when('/about/christiaan-westerbeek',     {templateUrl: 'partials'+p+'/about-chris.html',   controller: SiteCtrl}).
-      when('/news',                            {templateUrl: 'partials'+p+'/news.html',   controller: SiteCtrl}).
-      when('/news-old',                        {templateUrl: 'partials'+p+'/news-old.html',   controller: SiteCtrl}).
-      when('/customers',                       {templateUrl: 'partials'+p+'/customers.html', controller: SiteCtrl}).
-      when('/contact',                         {templateUrl: 'partials'+p+'/about-devotis.html',   controller: SiteCtrl}).
+      when('/',                                {templateUrl: p+'/home.html',    controller: SiteCtrl}).
+      when('/services/stoic',                  {templateUrl: p+'/stoic.html',   controller: SiteCtrl}).
+      when('/services/clang',                  {templateUrl: p+'/clang.html',   controller: SiteCtrl}).
+      when('/services/google-apps',            {templateUrl: p+'/google-apps.html',   controller: SiteCtrl}).
+      when('/services/javascript-development', {templateUrl: p+'/javascript-development.html',   controller: SiteCtrl}).
+      when('/services/sencha',                 {templateUrl: p+'/javascript-development.html',   controller: SiteCtrl}).
+      when('/services/domain-and-hosting',     {templateUrl: p+'/domain-and-hosting.html',   controller: SiteCtrl}).
+      when('/about/devotis',                   {templateUrl: p+'/about-devotis.html',   controller: SiteCtrl}).
+      when('/about/christiaan-westerbeek',     {templateUrl: p+'/about-chris.html',   controller: SiteCtrl}).
+      when('/news',                            {templateUrl: p+'/news.html',   controller: SiteCtrl}).
+      when('/news-old',                        {templateUrl: p+'/news-old.html',   controller: SiteCtrl}).
+      when('/customers',                       {templateUrl: p+'/customers.html', controller: SiteCtrl}).
+      when('/contact',                         {templateUrl: p+'/about-devotis.html',   controller: SiteCtrl}).
       otherwise({redirectTo: p});
 }]);
