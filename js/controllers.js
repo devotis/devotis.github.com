5c18b85a4b56c949a9938d34762f5297c51e5085
Christiaan Westerbeek <chris@devotis.nl>
2013-04-21 00:03:21 +0300

    Full tracking should be working now
    http://programmingnerd.blogspot.nl/2012/11/adding-google-analytics-to-my-angularjs.html
    http://stackoverflow.com/questions/10713708/tracking-google-analytics-page-views-with-angular-js

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,6 @@
 /* Controllers */
 
 function SiteCtrl($scope, $window, $location) {
-  console.log(arguments);
   $scope.$on('$viewContentLoaded', function(event) {
     $window._gaq.push(['_trackPageview', $location.path()]);
   });
